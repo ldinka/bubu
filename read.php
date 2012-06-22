55a788a45295917f750fcf781ecff00c06fc581e
Dina Efremova <icedinka@gmail.com>
2012-06-22 16:06:01 +0400

    social: fb

--- read.php
+++ read.php
@@ -12,8 +12,9 @@ if (is_numeric($_GET["article_id"])) {
     if (!empty($article)) {
         $article = array_shift($article);
         if ($article["article_approved"]) {
+            $url_article = $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
             Template::showTemplate("header", array("title" => $article["article_name"]));
-            Template::showTemplate("read", array("article" => $article));
+            Template::showTemplate("read", array("article" => $article, "url_article" => $url_article));
             Template::showTemplate("footer");
         } else {
             header("Location: /");
--- templates/read.php
+++ templates/read.php
@@ -1,6 +1,19 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <p><a href="/moderation.php">&lt;&lt; Вернуться</a></p>
 <p>&nbsp;</p>
 <?=$article["article_content"];?>
+<div class="social_buttons">
+<html xmlns:fb="http://ogp.me/ns/fb#">
+<fb:like href="<?=$url_article?>" send="false" layout="button_count" width="150" show_faces="false" font="tahoma"></fb:like>
+</div>
+
 <p class="left author">Автор: <?=$article["article_author"]?></p>
 <p class="right date"><?=strftime("%e %b %Y", strtotime($article["article_date"]))?></p>
 <div class="clear"></div>
